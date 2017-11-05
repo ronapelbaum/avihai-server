47c1b3072f71ef81e324b32a9b792dcb63d4f74a
Ron Apelbaum <ronapelbaum@gmail.com>
2017-11-05 21:53:07 +0200

    post body

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+
+const logger =  (req, res, next) => {
+	console.log(Date.now(), req.method, req.url);
+	next();
+};
 
 const app = express();
 
-// app.use(bodyParser);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
-app.all('*', (req, res, next) => {
-	console.log(Date.now(), req.method, req.url);
-	next();
-});
+app.all('*', logger);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/data', (req, res) => res.send(`data: ${req.body}`));
+app.post('/data', (req, res) => res.send(`data: ${JSON.stringify(req.body)}`));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
